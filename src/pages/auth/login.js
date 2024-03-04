260eaab4325ef82435a9ce5c5ad5daa32ccc9a74
SebastianL2 <sebas16cely@gmail.com>
2024-03-04 13:54:13 -0500

    dede

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -77,8 +77,8 @@ const Page = () => {
   };
   const formik = useFormik({
     initialValues: {
-      email: 'admin5@emia2.com',
-      password: 'contraseña123',
+      email: '',
+      password: '',
       submit: null
     },
     validationSchema: Yup.object({
